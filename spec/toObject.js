311a2d6b75b76df185e0d9dfa089cee721c24409
theZieger <mail@eric-zieger.de>
2017-08-22 01:21:07 +0200

    add new tests to cover mapBy array and function option

--- spec/toObject.js
+++ spec/toObject.js
@@ -10,7 +10,7 @@ describe('utilities.toObject', function() {
             expect(utilities.toObject.bind(null, {})).toThrowError(TypeError);
         });
 
-        it("should fail because the mapBy not of type String and not falsy", function() {
+        it("should fail because the mapBy not of type String, Array or Function and not falsy", function() {
             expect(utilities.toObject.bind(null, [], {})).toThrowError(TypeError);
         });
     });
@@ -30,28 +30,53 @@ describe('utilities.toObject', function() {
         });
     });
 
-    describe('array of objects toObject', function() {
-        // test example from README.md
-        var news = [
-            {
-                id: 12001,
-                headline: 'Tiger goes limp',
-                subHeadline: 'Pulls out after 9 holes'
-            },{
-                id: 666,
-                headline: 'Croc has beef with cow',
-                subHeadline: ''
-            },{
-                id: 1337,
-                headline: 'Germans wurst at penalties',
-                subHeadline: 'New stats prove England are better from the spot'
-            }
-        ];
+    var news = [
+        {
+            id: 12001,
+            headline: 'Tiger goes limp',
+            subHeadline: 'Pulls out after 9 holes'
+        },{
+            id: 666,
+            headline: 'Croc has beef with cow',
+            subHeadline: ''
+        },{
+            id: 1337,
+            headline: 'Germans wurst at penalties',
+            subHeadline: 'New stats prove England are better from the spot'
+        }
+    ];
+
+    describe('array of objects toObject - string mapBy', function() {
         var newsObject = utilities.toObject(news, 'id');
-        // end test example from README.md
 
         it("should be able to access object by ID", function() {
             expect(newsObject[1337].headline).toEqual('Germans wurst at penalties');
+        });
+
+        it("should have 3 keys", function() {
+            expect(Object.keys(newsObject).length).toEqual(3);
+        });
+    });
+
+    describe('array of objects toObject - array mapBy', function() {
+        var newsObject = utilities.toObject(news, ['id', 'id']);
+
+        it("should be able to access object by ID_ID", function() {
+            expect(newsObject['1337_1337'].headline).toEqual('Germans wurst at penalties');
+        });
+
+        it("should have 3 keys", function() {
+            expect(Object.keys(newsObject).length).toEqual(3);
+        });
+    });
+
+    describe('array of objects toObject - function mapBy', function() {
+        var newsObject = utilities.toObject(news, function(val, i, arr) {
+            return val.id + '_' + i;
+        });
+
+        it("should be able to access object by ID_index", function() {
+            expect(newsObject['1337_2'].headline).toEqual('Germans wurst at penalties');
         });
 
         it("should have 3 keys", function() {
