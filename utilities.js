1d5952db0195ca2d8db379199bbc4a5e2da131e7
theZieger <mail@eric-zieger.de>
2017-08-22 01:20:24 +0200

    adds support for multiple mapBy values (array, function) and combined ids

--- utilities.js
+++ utilities.js
@@ -17,10 +17,11 @@
 
     /**
      * inherit the prototype of the SuperConstructor
-     * 
-     * Warning: Changing the prototype of an object is, by the nature of how modern JavaScript engines
-     * optimize property accesses, a very slow operation, in every browser and JavaScript engine.
-     * So instead of using Object.setPrototypeOf or messing with __proto__, we create a new object
+     *
+     * Warning: Changing the prototype of an object is, by the nature of how
+     * modern JavaScript engines optimize property accesses, a very slow
+     * operation, in every browser and JavaScript engine. So instead of using
+     * Object.setPrototypeOf or messing with __proto__, we create a new object
      * with the desired prototype using Object.create().
      *
      * @see https://developer.mozilla.org/de/docs/Web/JavaScript/Reference/Global_Objects/Object/setPrototypeOf
@@ -28,7 +29,8 @@
      * @param {Object} Constructor
      * @param {Object} SuperConstructor
      *
-     * @throws {TypeError} if arguments are null/undefined, or SuperConstructor has no prototype
+     * @throws {TypeError} if arguments are `null`, `undefined`, or
+     *                     `SuperConstructor` has no prototype
      *
      * @returns {Void}
      */
@@ -45,7 +47,10 @@
             throw new TypeError('SuperConstructor.prototype is undefined');
         }
 
-        // for convenience, SuperConstructor will be accessible through the Constructor.super_ property
+        /**
+         * for convenience, `SuperConstructor` will be accessible through the
+         * `Constructor.super_` property
+         */
         Constructor.super_ = SuperConstructor;
 
         Constructor.prototype = Object.create(SuperConstructor.prototype);
@@ -53,37 +58,72 @@
     };
 
     /**
-     * Turns Array (of Objects) into associative Object (by given mapBy when given)
+     * Turns an Array into an associative Object (while keeping reference!)
      *
-     * @param {Array}  arr
-     * @param {String} mapBy  optional mapping key
+     * @param {Array}                 arr    Array of Objects to turn into an
+     *                                       associative Object
+     * @param {String|Array|Function} mapBy  optional mapping key, can be a
+     *                                       simple string (property name in
+     *                                       the abjects of arr), a list of
+     *                                       property names (which are
+     *                                       combined) or a function which
+     *                                       returns a unique id to use
      *
-     * @throws {TypeError} if arr is not an Array or mapBy is set but not a String
+     * @throws {TypeError} if arr is not an Array or mapBy is set but not of
+     *                     correct type (String, Array, Function)
      *
      * @returns {Object}
      */
     var toObject = function(arr, mapBy) {
         var obj = {};
-      
+
         if (!Array.isArray(arr)) {
             throw new TypeError('arr argument is not of type Array');
         }
 
-        if (mapBy !== undefined && typeof mapBy !== 'string') {
-            throw new TypeError('mapBy argument is not of type String');
+        if (mapBy !== undefined
+            && typeof mapBy !== 'string'
+            && !Array.isArray(mapBy)
+            && typeof mapBy !== 'function'
+        ) {
+            throw new TypeError(
+                'mapBy argument is not of type {String|Array|Function}'
+            );
         }
 
-        arr.forEach(function(val, i) {
-            if (!mapBy) {
-                obj[i] = val;
-            } else if (
-                typeof val[mapBy] === 'string'
-                || typeof val[mapBy] === 'number'
-            ) {
-                obj[val[mapBy]] = val;
+        var methods = {
+            string: function(val) {
+                this.undefined(val, val[mapBy]);
+            },
+            object: function(val, i) {
+                var newKey = mapBy.map(function(propertyName){
+                    return val[propertyName];
+                }).join('_');
+
+                this.undefined(val, newKey);
+            },
+            function: function(val, i, arr) {
+                this.undefined(val, mapBy(val, i, arr));
+            },
+            undefined: function(val, newKey) {
+                if (typeof newKey === 'string'
+                    || typeof newKey === 'number'
+                ) {
+                    obj[newKey] = val;
+                }
             }
-        });
-      
+        };
+
+        /**
+         * run the designated method by mapBy type from the methods object
+         * it binds the methods object so we can use the undefined setter method
+         * for different mapBy types and don't have to maintain multiple but
+         * same conditions
+         */
+        arr.forEach(
+            methods[(typeof mapBy)].bind(methods)
+        );
+
         return obj;
     };
 
